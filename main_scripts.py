8a452d0bc9e0b3114b48f21822e8ddcee4574d9e
michelet <michelet@usp.br>
2022-06-01 11:35:26 -0300

    adding utm files

--- main_scripts.py
+++ main_scripts.py
@@ -1,5 +1,47 @@
-from scripts import get_dynamic_soup, list_links
+from scripts import count_header_and_main, get_link_list
+import json
 
+#list_links('https://docs.blockbit.com/display/RC/Blockbit+GSM+-+Guia+do+Administrador')
+link1 = ('https://docs.blockbit.com/display/RC/Blockbit+UTM+-+Guia+do+Administrador','./Blockbit_UTM.html')
+link2 = ('https://docs.blockbit.com/display/RC/Blockbit+GSM+-+Guia+do+Administrador','./Blockbit_GSM.html')
 
-list_links('https://docs.blockbit.com/display/RC/Blockbit+GSM+-+Guia+do+Administrador')
+l1 = get_link_list(link1[0],link1[1])
+l2 = get_link_list(link2[0],link2[1])
 
+total_count = 0
+
+for item in l1:
+    print(item['url'])
+    url,counter,header,contents = count_header_and_main(item['url'])
+    item['count'] = counter
+    total_count += counter
+    try:
+        title = item["title"].replace('/','_')
+        with open(f'utm/{title}.txt', 'w+',encoding='utf8') as f:
+            f.write(f'{url}\n')
+            f.write(f'{counter}\n')
+            f.write(f'{header}\n')
+            f.write(f'{contents}\n')
+    except:
+        print(url)
+with open('utm.json','w+',encoding='utf8') as fp:
+    json.dump(l1, fp)
+
+for item in l2:
+    print(item['url'])
+    url,counter,header,contents = count_header_and_main(item['url'])
+    item['count'] = counter
+    total_count += counter
+    title = item["title"].replace('/','_')
+    try:
+        with open(f'gsm/{title}.txt', 'w+',encoding='utf8') as f:
+            f.write(f'{url}\n')
+            f.write(f'{counter}\n')
+            f.write(f'{header}\n')
+            f.write(f'{contents}\n')
+    except:
+        print(url)
+with open('gsm.json','w+',encoding='utf8') as fp:
+    json.dump(l2, fp)
+
+print(total_count)
--- scripts.py
+++ scripts.py
@@ -1,7 +1,7 @@
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
+import json
 from collections import Counter
 from string import punctuation
 from nltk.tokenize import RegexpTokenizer
@@ -14,20 +14,43 @@ def get_dynamic_soup(url: str):
         page = browser.new_page()
         page.goto(url)
         soup = BeautifulSoup(page.content(), "html.parser")        
-        browser.close()
-        print(soup)
+        browser.close()      
         return soup
 
-def list_links(base_url):
-    bsObj = get_dynamic_soup(base_url)
+def save_to_file(sopa):
+    with open('pagina1.html', 'wb+') as f:
+        f.write(sopa.content)
+
+def get_from_file(arquivo):
+    with open(arquivo) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+        return soup
+
+def get_link_list(base_url,arquivo):
+    #bs_obj = get_dynamic_soup(base_url)
+    bs_obj = get_from_file(arquivo)
+    # save_to_file(bs_obj)
     #bsObj = BeautifulSoup(html.read(), features="html.parser");
-    link_container = bsObj.find_all('span', class_='plugin_pagetree_children_span')
-    # link_container = bsObj.find_all('a')
-    print(link_container)
+    link_container = bs_obj.find_all('span', class_='plugin_pagetree_children_span')
+    
+    lista_links = []
+    count = 0
+    for l in link_container:
+        item = dict()
+        link = l.find_all('a',href=True)
+        url = link[0]['href']
+        title = link[0].text
+        item['id'] = count
+        item['url'] = url
+        item['title'] = title
+        lista_links.append(item)
+        count = count + 1
+    return lista_links
 
 def count_header_and_main(base_url):
     #base_url = 'https://docs.blockbit.com/display/RC/Blockbit+Client'
     html = urlopen(base_url)
+
     bsObj = BeautifulSoup(html.read(), features="html.parser");
 
     wiki_content = bsObj.find_all("div",class_='wiki-content')
@@ -35,17 +58,19 @@ def count_header_and_main(base_url):
     headers = main_header[0].get_text().split('\n')
     contents = wiki_content[0].get_text().split('\n')
     contador = 0
-    for c in contents:
-        tokenizer = RegexpTokenizer(r'\w+')
-        bla = tokenizer.tokenize(c)
-        contador += len(bla)
-        
+    texto = []
     for h in headers:
         tokenizer = RegexpTokenizer(r'\w+')
         bla = tokenizer.tokenize(h)
         contador += len(bla)
-    print(contador)
-    return (base_url,contador)
+        texto.append(bla)
+    for c in contents:
+        tokenizer = RegexpTokenizer(r'\w+')
+        bla = tokenizer.tokenize(c)
+        contador += len(bla)
+        texto.append(bla)
+    return (base_url,contador,headers,contents)
+
     
     
 
